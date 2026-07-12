26f36a196ae4ad1f82b50f7022f9a074a942a444
vanessakittivo <wambuinessie@gmail.com>
2026-07-12 20:06:33 +0300

    Fix palindrome linked list solution

--- python-data_structures/13-is_palindrome.c
+++ python-data_structures/13-is_palindrome.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * reverse_list - reverses a linked list
- * @head: pointer to head
+ * @head: head of list
+ *
  * Return: new head
  */
 listint_t *reverse_list(listint_t *head)
 {
 	listint_t *prev = NULL;
-	listint_t *next;
+	listint_t *next = NULL;
 
 	while (head)
 	{
@@ -18,17 +18,22 @@ listint_t *reverse_list(listint_t *head)
 		prev = head;
 		head = next;
 	}
+
 	return (prev);
 }
 
 /**
- * is_palindrome - checks if linked list is palindrome
- * @head: pointer to pointer to head
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head pointer
+ *
  * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow, *fast, *second;
+	listint_t *slow, *fast;
+	listint_t *second, *copy_second;
+	listint_t *first;
+	int palindrome = 1;
 
 	if (head == NULL || *head == NULL)
 		return (1);
@@ -42,15 +47,26 @@ int is_palindrome(listint_t **head)
 		fast = fast->next->next;
 	}
 
+	if (fast != NULL)
+		slow = slow->next;
+
 	second = reverse_list(slow);
+	copy_second = second;
+	first = *head;
 
 	while (second)
 	{
-		if ((*head)->n != second->n)
-			return (0);
-		*head = (*head)->next;
+		if (first->n != second->n)
+		{
+			palindrome = 0;
+			break;
+		}
+
+		first = first->next;
 		second = second->next;
 	}
 
-	return (1);
+	reverse_list(copy_second);
+
+	return (palindrome);
 }
