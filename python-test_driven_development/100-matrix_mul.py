1f91f37cd7cbc347a01f00672ae2a33a59ac9fc0
vanessakittivo <wambuinessie@gmail.com>
2026-08-11 22:22:27 +0300

    Fix matrix multiplication tasks

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for multiplying two matrices."""
+"""Module for matrix multiplication."""
 
 
 def matrix_mul(m_a, m_b):
@@ -10,6 +10,12 @@ def matrix_mul(m_a, m_b):
 
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
 
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
@@ -32,14 +38,10 @@ def matrix_mul(m_a, m_b):
                 )
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError(
-            "each row of m_a must should be of the same size"
-        )
+        raise TypeError("each row of m_a must be of the same size")
 
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError(
-            "each row of m_b must should be of the same size"
-        )
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
